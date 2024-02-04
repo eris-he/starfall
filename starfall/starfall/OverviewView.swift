1002adafc9e878210fe818e820c31c0832adb59a
Auburn University Student <austudent@LIB-CIRC-AIR-M1-32.local>
2024-02-03 18:38:46 -0600

    Overview page

--- starfall/starfall/OverviewView.swift
+++ starfall/starfall/OverviewView.swift
@@ -1,3 +1,6 @@
+#Preview {
+    OverviewView()
+}
 //
 //  OverviewView.swift
 //  starfall
@@ -9,10 +12,61 @@ import SwiftUI
 
 struct OverviewView: View {
     var body: some View {
-        Text("Overview Page")
+        
+        ZStack {
+            Color("bg-color")
+                .ignoresSafeArea()
+            
+            VStack(spacing: 20) {
+                Spacer()
+                
+                HStack {
+                    Image("planet-1") // Replace with the actual name of your first image asset
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                            
+                    Image("planet-1") // Replace with the actual name of your second image asset
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                        .offset(y: -20)
+                            
+                    Image("planet-1") // Replace with the actual name of your third image asset
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                            
+                        // Add more images as needed
+                }
+                
+                // Star garden area with the separate "Focus" button
+                Text("Completed Tasks: 5")
+                    .foregroundColor(.white)
+                    .font(.custom("Futura-Medium", size:28))
+                    
+                
+                Text("Scheduled Tasks: 10")
+                    .foregroundColor(.white)
+                    .font(.custom("Futura-Medium", size:28))
+                
+                ZStack {
+                    Image("planet-1") // Replace with the actual name of your first image asset
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 400, height: 400)
+                    
+                    Text("50%")
+                        .foregroundColor(.white)
+                        .shadow(color: .black, radius: 3)
+                        .bold()
+                        .font(.custom("Futura-Medium", size: 64))
+                }
+                
+                Text("Productivity")
+                    .foregroundColor(.white)
+                    .font(.custom("Futura-Medium", size:48))
+            }
+        }
     }
-}
-
-#Preview {
-    OverviewView()
 }
