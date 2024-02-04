c627abcd5492cb4a5e3e0f47ba83d15ffebbfc5d
Logan Bolton <loganbolton101@gmail.com>
2024-02-04 07:44:31 -0600

    health

--- starfall/starfall/HealthView.swift
+++ starfall/starfall/HealthView.swift
@@ -1,14 +1,8 @@
-//
-//  HealthView.swift
-//  starfall
-//
-//  Created by Eris He on 2/3/24.
-//
-
 import SwiftUI
 
 struct HealthView: View {
     @State private var userInput: String = ""
+    @State private var sleepRating: Int = 5
     @State private var isSaved: Bool = false
 
     var body: some View {
@@ -16,63 +10,92 @@ struct HealthView: View {
             Color("bg-color") // Background color
             
             VStack {
+                
+                Spacer()
+                
+                // PNG image with transparent background
+                ZStack {
+                    Image("sun") // Replace "your_image" with the name of your PNG image asset
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 360, height: 360)
+                        .padding()
+                        .cornerRadius(10)
+                        .shadow(color: .blue, radius: 10, x: 0, y: 0) // Add shadow effect
+                    
+                }
+                
                 Text("How are you feeling today?")
                     .font(.title)
                     .foregroundColor(.white)
-                    .padding(.top, 100) // Move the text up by adding top padding
+                    .padding(.top, 10) // Move the text up by adding top padding
 
                 TextField("Enter here", text: $userInput)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding()
                 
+                Text("How was your sleep?")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding(.top, 10) // Move the text up by adding top padding
+                
+                Picker(selection: $sleepRating, label: Text("Sleep Rating")) {
+                    ForEach(1..<11) { rating in
+                        Text("\(rating)")
+                            .foregroundColor(.black) // Set text color to black
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle()) // Use segmented picker style
+                .padding()
+                .foregroundColor(.white)
+                .background(Color.indigo)
                 Button(action: {
-                                    saveToFile()
-                                }) {
-                                    Text("Save")
-                                        .foregroundColor(.white)
-                                        .padding()
-                                        .background(Color.indigo)
-                                        .cornerRadius(10)
-                                }
-                                .padding()
-                                .disabled(userInput.isEmpty) // Disable button if user input is empty
+                    saveToFile()
+                }) {
+                    Text("Save")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.indigo)
+                        .cornerRadius(10)
+                }
+                .padding()
+                .disabled(userInput.isEmpty) // Disable button if user input is empty
 
-                                if isSaved {
-                                    Text("Data saved successfully")
-                                        .foregroundColor(.white)
-                                        .padding()
-                                } else {
-                                    Text("Data was not saved")
-                                        .foregroundColor(.white)
-                                        .padding()
-                                }
+                if isSaved {
+                    Text("Data saved successfully")
+                        .foregroundColor(.white)
+                        .padding()
+                }
+                Spacer()
             }
         }
         .edgesIgnoringSafeArea(.all)
     }
+    
     private func saveToFile() {
-        let fileName = "user_input.txt"
+        guard !isSaved else {
+            print("File already saved")
+            return
+        }
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMdd-HHmmss" // Define the date format
+
+        let fileName = "Health-\(formatter.string(from: Date())).txt"
         let fileURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent(fileName)
 
         do {
-            try userInput.write(to: fileURL, atomically: true, encoding: .utf8)
+            let data = "\(userInput)\nSleep Rating: \(sleepRating)"
+            try data.write(to: fileURL, atomically: true, encoding: .utf8)
             print("Data saved to file: \(fileURL)")
             isSaved = true
 
             // Reset isSaved after a delay
-            Timer.scheduledTimer(withTimeInterval: 600, repeats: false) { timer in
-                        isSaved = false
+            Timer.scheduledTimer(withTimeInterval: 6.0, repeats: false) { timer in
+                isSaved = false
             }
         } catch {
             print("Error saving data to file: \(error)")
         }
-    }
-
-
-}
-
-struct HealthView_Previews: PreviewProvider {
-    static var previews: some View {
-        HealthView()
     }
 }
